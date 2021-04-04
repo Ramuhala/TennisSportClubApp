25a9b4000a0a099c2581245b116e154601253633
Ramuhala <rofhiwa971115@gmail.com>
2021-04-04 20:03:53 +0200

    TennisSportClubApplication using java
    TennisSportClubApplication using java

---
+++ TennisSportsClubApp/src/tennissportsclub/TennisSportsClub.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tennissportsclub;
+
+/**
+ *
+ * @author Rofhiwa Ramuhala
+ */
+public class TennisSportsClub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmLogin frmL = new frmLogin();
+        frmL.setVisible(true);
+
+        
+                
+               
+    }
+    
+}
